411506d9986c1f51a15c830513bf7c2d2081f3f2
Mark Muskardin <markmuskardin@gmail.com>
2011-06-01 14:21:54 -0700

    jade templating is working

--- web.js
+++ web.js
@@ -1,9 +1,22 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
+var path = require('path');
+var jade = require('jade');
+// app.set('view engine', 'jade'); // Set jade as default render engine
+var everyone = require('now').initialize(app);
+
+app.configure(function(){
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
+  app.set('view options');
+});
+
 
 app.get('/', function(request, response) {
-  response.send('Hello smooth summer!');
+  everyone.now.msg = "Hello World!";
+  response.render('index', {
+      locals: {some: 'Locals'}
+  });
 });
 
 var port = process.env.PORT || 3000;
