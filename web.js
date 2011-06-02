fe0f2149b6421dea8b4336d0331b2f3ba0e52cca
Mark Muskardin <markmuskardin@gmail.com>
2011-06-01 20:04:00 -0700

    nowjs is now working

--- web.js
+++ web.js
@@ -2,18 +2,18 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var path = require('path');
 var jade = require('jade');
-// app.set('view engine', 'jade'); // Set jade as default render engine
 var everyone = require('now').initialize(app);
 
 app.configure(function(){
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'jade');
   app.set('view options');
+  var oneYear = 31557600000;
+  app.use("/", express.static(__dirname + '/public'));
 });
 
-
 app.get('/', function(request, response) {
-  everyone.now.msg = "Hello World!";
+  everyone.now.msg = "Hello Yada Mark and Dan!!";
   response.render('index', {
       locals: {some: 'Locals'}
   });
